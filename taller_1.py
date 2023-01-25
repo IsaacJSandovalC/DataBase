60ed1912374e191c96fe78c0b688a2ec8ba0cd7a
Isaac Sandoval <108201006+IsaacJSandovalC@users.noreply.github.com>
2023-01-25 18:17:07 -0500

    Update taller_1.py

--- taller_1.py
+++ taller_1.py
@@ -23,35 +23,34 @@ def print_all_data():
     result = cur.fetchall()
     for row in result:
         print(row)
-
-bd = sqlite3.connect('slangs.db')
-cur = bd.cursor()
-
-cur.execute("""
-    CREATE TABLE IF NOT EXISTS slangs (
-        id INTEGER PRIMARY KEY,
-        palabra TEXT,
-        significado TEXT)
-    """)
-
-slangs = {
-    1: {"palabra": "que xopa", "significado": "expresión utilizada para saludar"},
-    2: {"palabra": "yalabetia", "significado": "accion y efecto de sorpresa"},
-    3: {"palabra": "chantin", "significado": "referencia al domicilio de alguna persona"},
-    4: {"palabra": "fren", "significado": "haciendo referencia a algun amigo o conocido"},
-    5: {"palabra": "lapesillo", "significado": "haciendo referencia a que la persona es un joven"}
-}
-
-for slang in slangs.values():
-    cur.execute("SELECT * FROM slangs WHERE palabra=?", (slang["palabra"],))
-    if cur.fetchone() is None:
-        cur.execute("INSERT INTO slangs (palabra, significado) VALUES (?, ?)",
-                    (slang["palabra"], slang["significado"]))
-
-bd.commit()
-
-valor = True
 try:
+    bd = sqlite3.connect('slangs.db')
+    cur = bd.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS slangs (
+            id INTEGER PRIMARY KEY,
+            palabra TEXT,
+            significado TEXT)
+        """)
+
+    slangs = {
+        1: {"palabra": "que xopa", "significado": "expresión utilizada para saludar"},
+        2: {"palabra": "yalabetia", "significado": "accion y efecto de sorpresa"},
+        3: {"palabra": "chantin", "significado": "referencia al domicilio de alguna persona"},
+        4: {"palabra": "fren", "significado": "haciendo referencia a algun amigo o conocido"},
+        5: {"palabra": "lapesillo", "significado": "haciendo referencia a que la persona es un joven"}
+    }
+
+    for slang in slangs.values():
+        cur.execute("SELECT * FROM slangs WHERE palabra=?", (slang["palabra"],))
+        if cur.fetchone() is None:
+            cur.execute("INSERT INTO slangs (palabra, significado) VALUES (?, ?)",
+                        (slang["palabra"], slang["significado"]))
+
+    bd.commit()
+    valor = True
+
     while valor:
         print('--------------------------------------------------------------')
         print('Este es el menu de nuestrA base de datos, ingresa tu accion:\n'
